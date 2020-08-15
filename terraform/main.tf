447ff65ee4167e07a85d0ea8fe9432564fd69a3f
Truls <Truls.stenrud@me.com>
2020-08-15 16:54:54 +0200

    Do not have policy in file

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,24 @@ provider "aws" {
     region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "b" {
+resource aws_s3_bucket b {
   bucket = "trulsstenrud.no"
   acl    = "public-read"
-  policy = file("policy.json")
-
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Id": "IDSADAS",
+    "Statement": [
+        {
+            "Sid": "AddPerm",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::trulsstenrud.no/*"
+        }
+        ]
+    }
+POLICY
   website {
     index_document = "index.html"
     error_document = ""
