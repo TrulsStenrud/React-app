a01f48cbc447fb618885d43d214ef41c38fcf3e4
Truls <Truls.stenrud@me.com>
2020-12-21 13:32:18 +0100

    Fancy sliding is working
    Currently no styling, normal button and white background

--- src/ManuOverlay/MenuOverlay.tsx
+++ src/ManuOverlay/MenuOverlay.tsx
@@ -6,9 +6,14 @@ const MenuOverlay = (props: { children: any }) => {
 
     var [isOpen, setOpen] = useState(false)
     const toggleOpen = () => setOpen(!isOpen)
+    
+    const visibility = isOpen ? "show" : "hide"
 
     return <>
-        <button className="menuBtn">
+        <div id="slidingMenu" className={visibility}>
+            This is my sliding menu
+        </div>
+        <button id="menuBtn" onClick={toggleOpen}>
             Menu button
         </button>
         <div className="Content">
